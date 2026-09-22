c822963d2fb780c3d06f867ff7937a69b4a28dc1
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-09-23 02:13:42 +0900

    17.4

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -447,11 +447,13 @@ EFI_STATUS EFIAPI UefiMain(
     }
   }
 
-  typedef void __attribute__((sysv_abi)) EntryPointType(const struct FrameBufferConfig*,
+  typedef void __attribute__((sysv_abi)) EntryPointType(
+                              const struct FrameBufferConfig*,
                               const struct MemoryMap*,
-                              const VOID*);
+                              const VOID*,
+                              VOID*);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point(&config, &memmap, acpi_table);
+  entry_point(&config, &memmap, acpi_table, volume_image);
 
   Print(L"All done\n");
 
